fd72a56e20edb391040ce15ec7562be9ffe089b8
MishalPandey <mishalpandey1316@gmail.com>
2026-08-15 17:50:55 +0530

    Initial commit

---
+++ Docker-Practice-Notes/2_Docker-Containerizing/hello-spring/src/main/java/devops/mishal/hello_spring/HelloController.java
@@ -0,0 +1,18 @@
+package devops.mishal.hello_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public Map<String, String> hello() throws Exception {
+        String env = System.getenv().getOrDefault("ENV_VALUE", "No env set");
+        String hostname = InetAddress.getLocalHost().getHostName();
+
+        return Map.of("message", "Hello from Simple App (Spring Boot)", "env", env, "container", hostname);
+    }
+}
